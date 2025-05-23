7d054b06a6a44fba3821820a17f692903649cb74
cat-x <a599231042@163.com>
2025-05-23 11:19:17 +0800

    fix 0.1.0

--- src/Executorch.h
+++ src/Executorch.h
@@ -11,8 +11,8 @@
 #include <executorch/ExecuTorchValue.h>
 
 // If using C++ directly alongside Objective-C++, you might still need C++ headers.
-#include <executorch/extension/module/module.h>
-#include <executorch/extension/tensor/tensor.h>
+//#include <executorch/extension/module/module.h>
+//#include <executorch/extension/tensor/tensor.h>
 
 
 #endif
